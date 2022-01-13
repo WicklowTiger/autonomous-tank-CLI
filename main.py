09650db7bf26865a912237d0ce52c66b2e2d84aa
WicklowTiger <razvan.miculit@gmail.com>
2022-01-13 20:58:57 +0200

    feat: CLI 0.1

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+import argparse
+
+from src.SocketSender import SocketSender
+from src.config import *
+
+parser = argparse.ArgumentParser(description='Autonomous Military Vehicle CLI.')
+parser.add_argument('--wireless', action="store_true", help="uses wireless connection instead of wired")
+group = parser.add_mutually_exclusive_group()
+group.add_argument('--live', action="store_true", help='start live command processing')
+group.add_argument('--msg', type=str, help='send a message to the tank control board')
+
+args = parser.parse_args()
+
 if __name__ == "__main__":
-    print("Hello from autonomous-tank-CLI!")
+    socket_sender = SocketSender(WIRELESS_IP, WIRELESS_PORT) if args.wireless else SocketSender(UDP_IP, UDP_PORT)
+    if args.live:
+        while True:
+            command = str(input())
+            if command.lower() in ["stop", "q"]:
+                print("Process terminated successfully!")
+                break
+            else:
+                socket_sender.send_message(command)
+    elif args.msg:
+        socket_sender.send_message(args.msg)
+    else:
+        parser.print_help()
 
